a693914e1d33308e366421ccd36a796f905bc2ba
Katia Moreira <ktia.morei@gmail.com>
2024-02-13 21:28:29 +0100

    Add final version

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,11 @@
 import '../scss/app.scss';
+import '../../index.html';
+import Splitting from 'splitting';
 import jobs from './jobs';
 
 document.addEventListener('DOMContentLoaded', () => {
   const titles = ['code lover', 'coffee addict', 'front-end web developer'];
   jobs(titles);
+
+  Splitting();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/assets/js/app.js',
-  mode: 'production',
-  watch: true,
   output: {
     path: path.resolve('./dist'),
     // ou : path: path.resolve(__dirname, 'dist')
@@ -13,14 +12,18 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
+        use: { 
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env']
@@ -31,8 +34,15 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'sass-loader'
         ]
+      }, {
+        test: /\.html$/,
+        use: 'html-loader'
+      }, {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: 'image-webpack-loader'
       }
     ]
   }
